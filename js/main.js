71bddb17e6daafe1d77b9ecdc3acaf529f7f8e3c
Akbar <jihad.akbar.ar@gmail.com>
2024-08-26 17:55:34 +0700

    add swiper, still need fix

--- js/main.js
+++ js/main.js
@@ -1,25 +1,56 @@
-const tabItems = document.querySelectorAll('.tab-item');
-const tabContentItem = document.querySelectorAll('.tab-content-item');
+var swiper1 = new Swiper('.swiper1', {
+    loop: true, // Enable looping through slides
+    navigation: {
+        nextEl: '.swiper-button-next1',
+        prevEl: '.swiper-button-prev1',
+    },
+    pagination: {
+        el: '.swiper-pagination1', // Pagination element
+        clickable: true, // Make pagination bullets clickable
+    },
+    slidesPerView: 3, // Adjust based on the number of slides to show at once
+    spaceBetween: 20, // Space between slides
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+    }
+});
 
-//Select tab content item
-function selectItem(e) {
-    removeBorder();
-    removeShow();
-    //Add border to current tab
-    this.classList.add('tab-border');
-    //Grab Content Item From DOM
-    const tabContentItem = document.querySelector(`#${this.id}-content`);
-    //Add Show class
-    tabContentItem.classList.add('show');
-}
 
-function removeBorder() {
-    tabItems.forEach(item => item.classList.remove('tab-border'));
-}
-
-function removeShow() {
-    tabContentItem.forEach(item => item.classList.remove('show'));
-}
-
-// Listen For tab Click
-tabItems.forEach(item => item.addEventListener('click', selectItem));
+var swiper2 = new Swiper('.swiper2', {
+    loop: true, // Enable looping through slides
+    navigation: {
+        nextEl: '.swiper-button-next2',
+        prevEl: '.swiper-button-prev2',
+    },
+    pagination: {
+        el: '.swiper-pagination2', // Pagination element
+        clickable: true, // Make pagination bullets clickable
+    },
+    slidesPerView: 4, // Adjust based on the number of slides to show at once
+    spaceBetween: 20, // Space between slides
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+    }
+});
